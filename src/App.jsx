879e26a079e525d7b51c33d7d77830c26279c2e5
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-01 09:31:39 +0000

    Added States

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ import {
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
+const[loading,setLoading]=useState(true)
+  const[person,setPerson]=useState(null)
+const [title,setTitle]=useState('name')
+const [value,setValue]=useState('random person')
+
+  
   return <h2>random user starter</h2>
 }
 
