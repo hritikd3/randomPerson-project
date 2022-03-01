eecdce37691d3543b3c0ef212cad877b75c73028
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-01 13:03:01 +0000

    rechangees in css

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,8 @@ function App() {
   }
   return (
     <main>
+
+      <div className="c"> Generate the Random You </div>
       <div className='block bcg-black'></div>
       <div className='block'>
         <div className='container'>
