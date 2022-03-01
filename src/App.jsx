d827ea92b89f1ac8263e3ee992de9cb603b6b35b
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-01 12:27:53 +0000

    added button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,42 @@ import {
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/85.jpg'
 function App() {
-const[loading,setLoading]=useState(true)
-  const[person,setPerson]=useState(null)
-const [title,setTitle]=useState('name')
-const [value,setValue]=useState('random person')
+  const [loading, setLoading] = useState(true)
+  const [person, setPerson] = useState(null)
+  const [value, setValue] = useState('random person')
+  const [title, setTitle] = useState('name')
+  const getPerson = async () => {
+    setLoading(true)
+    const response = await fetch(url)
+    const data = await response.json()
+    const person = data.results[0]
+    const { phone, email } = person
+    const { large: image } = person.picture
+    const { password } = person.login
+    const { first, last } = person.name
+    const {
+      dob: { age },
+    } = person
+    const {
+      street: { number, name },
+    } = person.location
 
-const getPerson=async ()=>{
-  const response= await fetch(url);
- const data = await response.json();
-  const person =data.results[0];
-  const {phone,email} = person;
-  const {large:image} = person.picture;
-  const {login: password}, = person;
-}
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    }
+    setPerson(newPerson)
+    setLoading(false)
+    setTitle('name')
+    setValue(newPerson.name)
+  }
+
+
   useEffect(()=>{
       getPerson();
   },[])
@@ -37,19 +60,19 @@ const getPerson=async ()=>{
 <div className="container">
 <img src={(person && person.image) || defaultImage} alt="random user" className="user-img"/>
 <p className="user-title" > my {title} is </p>
-<p className="user-value" > my {value} is </p>
+<p className="user-value" > {value} </p>
   <div className="values-list">
 
 <button className="icon" data-label="user" onMouseOver={handleValue} > <FaUser /></button>
 <button className="icon" data-label="email" onMouseOver={handleValue} > <FaEnvelopeOpen /></button>
 <button className="icon" data-label="age" onMouseOver={handleValue} > <FaCalendarTimes /></button>
-<button className="icon" data-label="address" onMouseOver={handleValue} > <FaMap /></button>
+<button className="icon" data-label="street" onMouseOver={handleValue} > <FaMap /></button>
 <button className="icon" data-label="phone" onMouseOver={handleValue} > <FaPhone /></button>
 <button className="icon" data-label="password" onMouseOver={handleValue}> <FaLock /></button >
 
 
   </div>
-<button className="btn" type="button"> { loading ? ' loading...' : 'random user'}</button >
+<button className="btn" type="button" onClick={getPerson}> { loading ? ' loading...' : 'random user'}</button >
 
   
   
